b593a5ac6ab12f98001bc7e88716125abe49305e
gregoiredechaine <gregdechaine@me.com>
2020-02-05 13:14:38 -0500

    Add in all of the project files.

---
+++ assets/plugins/contact/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Mail Setup
+// ==========
+
+// Email address notifications will be sent to
+$to_email = 'michele-anne@villabahati.com';
+
+// Email subject
+$mail_subject = 'Villa Bahati | Get In Touch With Us';
+
+// Email content can be modified in sendmail.php
+
+?>
