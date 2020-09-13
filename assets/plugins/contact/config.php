6da32c364f265818bddbf3cbc0533772e57f9378
gregoiredechaine <gregoire@rabaglinoarchitect.com>
2020-09-13 15:38:05 -0400

    Update config.php

--- assets/plugins/contact/config.php
+++ assets/plugins/contact/config.php
@@ -4,7 +4,7 @@
 // ==========
 
 // Email address notifications will be sent to
-$to_email = 'michele-anne@villabahati.com';
+$to_email = 'micheleannevanderwal@gmail.com';
 
 // Email subject
 $mail_subject = 'Villa Bahati | Get In Touch With Us';
